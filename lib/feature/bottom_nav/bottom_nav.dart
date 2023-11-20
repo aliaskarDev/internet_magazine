52a4a637cb25d7911a23d7a2d7e9032fb404e5c8
Aliaskar Kerimkulow <kerimkulovaliaskar@gmail.com>
2023-11-20 10:16:51 +0600

    Bottom Navigation make with VlueNotifier

--- lib/feature/bottom_nav/bottom_nav.dart
+++ lib/feature/bottom_nav/bottom_nav.dart
@@ -4,7 +4,7 @@ import 'package:internet_magazine/constants/const.dart';
 import 'package:internet_magazine/constants/icons_path.dart';
 import 'package:internet_magazine/feature/catolog/catolog_screen.dart';
 import 'package:internet_magazine/feature/location/location_screen.dart';
-import 'package:internet_magazine/feature/main/void/main_screen.dart';
+import 'package:internet_magazine/feature/main/main_screen.dart';
 import 'package:internet_magazine/feature/profile/profile_screen.dart';
 import 'package:internet_magazine/ui/colors.dart';
 import 'package:internet_magazine/ui/text_style.dart';
@@ -17,7 +17,7 @@ class BottomNavigation extends StatefulWidget {
 }
 
 class BottomNavigationState extends State<BottomNavigation> {
-  int _selectedIndex = 0;
+  final ValueNotifier<int> _selectedTabIndex = ValueNotifier<int>(0);
 
   final List<Widget> _pages = [
     const MainScreen(),
@@ -28,73 +28,183 @@ class BottomNavigationState extends State<BottomNavigation> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: _pages[_selectedIndex],
-        bottomNavigationBar: Container(
-          height: 75,
-          decoration: const BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(
-                24,
+    return Scaffold(
+      body: ValueListenableBuilder<int>(
+        valueListenable: _selectedTabIndex,
+        builder: (context, selectedIndex, child) {
+          return _pages[selectedIndex];
+        },
+      ),
+      bottomNavigationBar: ValueListenableBuilder<int>(
+        valueListenable: _selectedTabIndex,
+        builder: (context, selectedIndex, child) {
+          return Container(
+            height: 75,
+            decoration: const BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(
+                  24,
+                ),
               ),
             ),
-          ),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.vertical(
-              top: Radius.circular(
-                24,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(
+                  24,
+                ),
+              ),
+              child: BottomNavigationBar(
+                showSelectedLabels: true,
+                showUnselectedLabels: true,
+                useLegacyColorScheme: false,
+                selectedLabelStyle: MyTextSTyle.style400w10.copyWith(
+                  color: MyColors.mySecondBlue,
+                ),
+                unselectedLabelStyle: MyTextSTyle.style400w10.copyWith(
+                  color: MyColors.mySecondBlue,
+                ),
+                items: [
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      MyIcons.main,
+                    ),
+                    label: BottomText.main,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      MyIcons.catolog,
+                    ),
+                    label: BottomText.catalog,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      MyIcons.location,
+                    ),
+                    label: BottomText.location,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: SvgPicture.asset(
+                      MyIcons.profile,
+                    ),
+                    label: BottomText.profile,
+                  ),
+                ],
+                currentIndex: selectedIndex,
+                onTap: (index) {
+                  _selectedTabIndex.value = index;
+                },
               ),
             ),
-            child: BottomNavigationBar(
-              showSelectedLabels: true,
-              showUnselectedLabels: true,
-              useLegacyColorScheme: false,
-              selectedLabelStyle: MyTextSTyle.style400w10.copyWith(
-                color: MyColors.mySecondBlue,
-              ),
-              unselectedLabelStyle: MyTextSTyle.style400w10.copyWith(
-                color: MyColors.mySecondBlue,
-              ),
-              items: [
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    MyIcons.main,
-                  ),
-                  label: BottomText.main,
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    MyIcons.catolog,
-                  ),
-                  label: BottomText.catalog,
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    MyIcons.location,
-                  ),
-                  label: BottomText.location,
-                ),
-                BottomNavigationBarItem(
-                  icon: SvgPicture.asset(
-                    MyIcons.profile,
-                  ),
-                  label: BottomText.profile,
-                ),
-              ],
-              currentIndex: _selectedIndex,
-              onTap: _onItemTapped,
-            ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 }
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class BottomNavigation extends StatefulWidget {
+//   const BottomNavigation({super.key});
+
+//   @override
+//   BottomNavigationState createState() => BottomNavigationState();
+// }
+
+// class BottomNavigationState extends State<BottomNavigation> {
+//   int _selectedIndex = 0;
+
+//   final List<Widget> _pages = [
+//     const MainScreen(),
+//     const CatologScreen(),
+//     const LocationScreen(),
+//     const ProfileScreen(),
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: _pages[_selectedIndex],
+//         bottomNavigationBar: Container(
+//           height: 75,
+//           decoration: const BoxDecoration(
+//             color: Colors.black,
+//             borderRadius: BorderRadius.vertical(
+//               top: Radius.circular(
+//                 24,
+//               ),
+//             ),
+//           ),
+//           child: ClipRRect(
+//             borderRadius: const BorderRadius.vertical(
+//               top: Radius.circular(
+//                 24,
+//               ),
+//             ),
+//             child: BottomNavigationBar(
+//               showSelectedLabels: true,
+//               showUnselectedLabels: true,
+//               useLegacyColorScheme: false,
+//               selectedLabelStyle: MyTextSTyle.style400w10.copyWith(
+//                 color: MyColors.mySecondBlue,
+//               ),
+//               unselectedLabelStyle: MyTextSTyle.style400w10.copyWith(
+//                 color: MyColors.mySecondBlue,
+//               ),
+//               items: [
+//                 BottomNavigationBarItem(
+//                   icon: SvgPicture.asset(
+//                     MyIcons.main,
+//                   ),
+//                   label: BottomText.main,
+//                 ),
+//                 BottomNavigationBarItem(
+//                   icon: SvgPicture.asset(
+//                     MyIcons.catolog,
+//                   ),
+//                   label: BottomText.catalog,
+//                 ),
+//                 BottomNavigationBarItem(
+//                   icon: SvgPicture.asset(
+//                     MyIcons.location,
+//                   ),
+//                   label: BottomText.location,
+//                 ),
+//                 BottomNavigationBarItem(
+//                   icon: SvgPicture.asset(
+//                     MyIcons.profile,
+//                   ),
+//                   label: BottomText.profile,
+//                 ),
+//               ],
+//               currentIndex: _selectedIndex,
+//               onTap: _onItemTapped,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
